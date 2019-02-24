5431268ce1ea68a4f9f592ede259070334796a68
tongsun1996 <18168829677@163.com>
2019-02-24 16:27:10 +0800

    pythontest

---
+++ README.md
@@ -0,0 +1,14 @@
+## Installation
+
+```
+$ pipenv install --dev
+$ pipenv shell
+$ flask forge
+$ flask run
+* Running on http://127.0.0.1:5000/
+```
+
+Test account:
+
+* username: `admin`
+* password: `123`
---
+++ tests/base.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from flask import url_for
+
+from flasktest import create_app
+from flasktest.extensions import db
+from flasktest.models import Admin
+
+
+class BaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app = create_app('testing')
+        self.context = app.test_request_context()
+        self.context.push()
+        self.client = app.test_client()
+        self.runner = app.test_cli_runner()
+
+        db.create_all()
+        user = Admin(name='Sun Tong', username='tong', about='I am test', blog_title='Testlog', blog_sub_title='a test')
+        user.set_password('123')
+        db.session.add(user)
+        db.session.commit()
+
+    def tearDown(self):
+        db.drop_all()
+        self.context.pop()
+
+    def login(self, username=None, password=None):
+        if username is None and password is None:
+            username = 'tong sun'
+            password = '1234'
+
+        return self.client.post(url_for('auth.login'), data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.client.get(url_for('auth.logout'), follow_redirects=True)
